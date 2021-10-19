e23adca9fae59da992dbe6984478e71fffaa86e7
Henri Sota <sotahenry@gmail.com>
2021-10-19 10:50:11 +0200

    Implement shade for ShaderMirror

--- src/ShaderMirror.h
+++ src/ShaderMirror.h
@@ -16,8 +16,22 @@ public:
 
 	virtual Vec3f shade(const Ray& ray) const override
 	{
-		// --- PUT YOUR CODE HERE ---
-		return Vec3f(0,0,0);
+        Vec3f shadingNormal = ray.hit->getNormal(ray);
+
+        // Calculate the reflected ray origin and direction
+        Ray reflected;
+
+        float cos_alpha = -ray.dir.dot(shadingNormal);
+
+        // The origin of the reflected ray is the hitpoint
+        reflected.org = ray.org + ray.dir * ray.t;
+
+        if (cos_alpha > 0)
+            reflected.dir = normalize(ray.dir + 2 * cos_alpha * shadingNormal);
+        else
+            reflected.dir = ray.dir;
+
+        return m_scene.RayTrace(reflected);
 	}
 	
 	
