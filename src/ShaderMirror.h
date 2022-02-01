31ace83986702c70e9e58b10f5769c08d924dbac
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-02-01 12:08:21 +0100

    Update ShaderMirror.h

--- src/ShaderMirror.h
+++ src/ShaderMirror.h
@@ -17,17 +17,46 @@ public:
 	virtual Vec3f shade(const Ray& ray) const override
 	{
 		// --- PUT YOUR CODE HERE ---
-        Vec3f N=ray.hit->getNormal(ray);
-        if (ray.dir.dot(N) > 0) {
-            N = N * -1;
-        }
-        Vec3f R = ray.dir - 2 * (ray.dir.dot(N)) * N;
-        Ray reflectedRay;
-        reflectedRay.dir = normalize(R);
-        reflectedRay.t = std::numeric_limits<float>::max();
-        reflectedRay.org = ray.dir * ray.t + ray.org;
-        return m_scene.RayTrace(reflectedRay);	}
-	
+ //flip surfaceNorm if on other side from incident ray
+                Vec3f surfaceNorm = ray.hit->getNormal(ray);
+                if (surfaceNorm.dot(ray.dir) > 0) {
+                    surfaceNorm *= -1;
+                }
+                
+                //calc ambient term
+                Vec3f ambient = m_ka * m_color;
+                
+                //calc defuse and specular terms
+                Vec3f defuse;
+                Vec3f specular;
+                std::vector<ptr_light_t> lights = m_scene.getLights();
+                for (auto light = lights.begin(); light != lights.end(); light++) {
+                    
+                    //get ray and intensity from light source
+                    Ray I;
+                    I.org = ray.org + ray.dir*ray.t;
+                    I.hit = ray.hit;
+                    Vec3f Li = light->get()->illuminate(I).value();
+                    
+                    if (!m_scene.occluded(I)) { //skip if light is blocked
+                        
+                        //incrament defuse and specular sums
+                        double IdotN = I.dir.dot(surfaceNorm);
+                        if (IdotN > 0) { //only consider front side
+                            defuse += Li * IdotN;
+                            
+                            double IdotR = I.dir.dot(normalize(ray.dir - ray.dir.dot(surfaceNorm) * surfaceNorm * 2));
+                            if (IdotR > 0) {
+                                specular += Li * pow(IdotR, m_ke);
+                            }
+                        }
+                    }
+                }
+                defuse = m_kd * m_color.mul(defuse);
+                specular = m_ks * Vec3f(1, 1, 1).mul(specular);
+                
+                return ambient + defuse + specular;
+	}
 	
 private:
 	CScene& m_scene;
